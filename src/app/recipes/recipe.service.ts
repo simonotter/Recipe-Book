70649afef16bab9d1e2561c53def906866f7023d
Simon Otter <simonotter@gmail.com>
2020-03-03 11:19:00 +0000

    Consumed Recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
 import { Recipe } from './recipe.model';
+import { EventEmitter } from '@angular/core';
 
 export class RecipeService {
+    recipeSelected = new EventEmitter<Recipe>();
+
     private recipes: Recipe[] = [
         new Recipe('A Test Recipe', 'This is simply a test',
         'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg'),
@@ -11,4 +14,6 @@ export class RecipeService {
     getRecipes() {
         return this.recipes.slice();
     }
+
+
 }
