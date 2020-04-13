36a07033e283f6c8065c36366f42f956e36c4435
Simon Otter <simonotter@gmail.com>
2020-04-13 18:35:50 +0100

    Commented out hard-coded recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -9,26 +9,29 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
     recipesChanged = new Subject<Recipe[]>();
 
-    private recipes: Recipe[] = [
-        new Recipe(
-            'A Test Recipe',
-            'This is simply a test',
-            'https://assets.bonappetit.com/photos/5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
-            [
-                new Ingredient('Meat', 1),
-                new Ingredient('French Fries', 20)
-            ]),
-        new Recipe(
-            'Piri-piri chicken with smashed sweet potatoes & broccoli',
-            'Serve up an easy one-pan traybake of piri-piri chicken with sweet potatoes and ' +
-            'broccoli. As well as being simple to make, it delivers three of your 5-a-day',
-            'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/' +
-                'recipe/recipe-image/2020/02/piri-piri-chicken-with-smashed-sweet-potatoes-broccoli.jpg?itok=2cCr3I7C',
-            [
-                new Ingredient('Buns', 2),
-                new Ingredient('Meat', 1)
-            ])
-    ];
+    // private recipes: Recipe[] = [
+    //     new Recipe(
+    //         'A Test Recipe',
+    //         'This is simply a test',
+    //         'https://assets.bonappetit.com/photos/' +
+    //             '5d7296eec4af4d0008ad1263/3:2/w_2560,c_limit/Basically-Gojuchang-Chicken-Recipe-Wide.jpg',
+    //         [
+    //             new Ingredient('Meat', 1),
+    //             new Ingredient('French Fries', 20)
+    //         ]),
+    //     new Recipe(
+    //         'Piri-piri chicken with smashed sweet potatoes & broccoli',
+    //         'Serve up an easy one-pan traybake of piri-piri chicken with sweet potatoes and ' +
+    //         'broccoli. As well as being simple to make, it delivers three of your 5-a-day',
+    //         'https://www.bbcgoodfood.com/sites/default/files/styles/recipe/public/' +
+    //             'recipe/recipe-image/2020/02/piri-piri-chicken-with-smashed-sweet-potatoes-broccoli.jpg?itok=2cCr3I7C',
+    //         [
+    //             new Ingredient('Buns', 2),
+    //             new Ingredient('Meat', 1)
+    //         ])
+    // ];
+    private recipes: Recipe[] = [];
+
 
     constructor(private shoppingListService: ShoppingListService) {}
 
