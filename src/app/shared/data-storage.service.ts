45fea843be007f9c2fcd4ed0692047dc127e6d99
Simon Otter <simonotter@gmail.com>
2020-04-13 18:03:43 +0100

    Stored recipes in Firebase

---
+++ src/app/shared/data-storage.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { RecipeService } from '../recipes/recipe.service';
+
+@Injectable({providedIn: 'root'})
+export class DataStorageService {
+    constructor(private http: HttpClient,
+                private recipesService: RecipeService) {}
+
+    storeRecipes() {
+        const recipes = this.recipesService.getRecipes();
+        this.http
+        .put(
+            'https://recipe-book-51af3.firebaseio.com/recipes.json',
+            recipes
+        )
+        .subscribe(response => {
+            console.log(response);
+        });
+    }
+}
