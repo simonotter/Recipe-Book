fd4ae3d97ac53ef495dda0609ba157cfdfa3ee67
Simon Otter <simonotter@gmail.com>
2020-04-15 19:40:01 +0100

    Added HttpInteceptor to send auth token

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -25,16 +25,11 @@ export class DataStorageService {
     }
 
     fetchRecipes() {
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>(
-                    'https://recipe-book-51af3.firebaseio.com/recipes.json',
-                    {
-                        params: new HttpParams().set('auth', user.token)
-                    }
-                );
-            }),
+        return this.http
+        .get<Recipe[]>(
+            'https://recipe-book-51af3.firebaseio.com/recipes.json'
+        )
+        .pipe(
             map(recipes => {
                 return recipes.map(recipe => {
                     return  {
