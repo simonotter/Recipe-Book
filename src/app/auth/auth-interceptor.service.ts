654aadad72920256820078b3f0fcf4a3cc6c2698
Simon Otter <simonotter@gmail.com>
2020-04-16 19:28:42 +0100

    Added comment around previous commit

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -17,7 +17,7 @@ export class AuthInterceptorService implements HttpInterceptor {
         return this.authService.user.pipe(
             take(1),
             exhaustMap(user => {
-                if (!user) {
+                if (!user) {    // don't send auth token if we don't yet have a user
                     return next.handle(req);
                 }
                 const modifiedReq = req.clone({
