d768ccdd6a65c0d5b8bb56bdc1d9b26362cbec3b
Simon Otter <simonotter@gmail.com>
2020-04-25 10:35:24 +0100

    Imported AppMaterialModule into shopping-list

--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { AppMaterialModule } from '../app-material/app-material.module';
 
 import { ShoppingListComponent } from './shopping-list.component';
 import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
@@ -15,6 +16,7 @@ import { ShoppingEditComponent } from './shopping-edit/shopping-edit.component';
     imports: [
         CommonModule,
         FormsModule,
+        AppMaterialModule,
         RouterModule.forChild([
             { path: 'shopping-list', component: ShoppingListComponent},
         ])
