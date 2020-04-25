49568912e0ab47605e86ebb25107ba561d474d4a
Simon Otter <simonotter@gmail.com>
2020-04-25 10:32:38 +0100

    Moved Shopping List to separate module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
-import { ShoppingListComponent } from './shopping-list/shopping-list.component';
-import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
@@ -20,12 +20,11 @@ import { MatHeaderComponent } from './mat-header/mat-header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { AppMaterialModule } from './app-material/app-material.module';
 
+
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
     DropdownDirective,
     AuthComponent,
     LoadingSpinnerComponent,
@@ -39,6 +38,7 @@ import { AppMaterialModule } from './app-material/app-material.module';
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
+    ShoppingListModule,
     BrowserAnimationsModule,
     LayoutModule,
     AppMaterialModule
