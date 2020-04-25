ae2c23e7476bb824b250429e460219340b4e4572
Simon Otter <simonotter@gmail.com>
2020-04-25 17:30:50 +0100

    Created CoreModule and imported it

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core.module';
 import { SharedModule } from './shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { RecipesModule } from './recipes/recipes.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
-import { ShoppingListService } from './shopping-list/shopping-list.service';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RecipeService } from './recipes/recipe.service';
 import { AuthComponent } from './auth/auth.component';
-import { AuthInterceptorService } from './auth/auth-interceptor.service';
 import { MatHeaderComponent } from './mat-header/mat-header.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { AppMaterialModule } from './app-material/app-material.module';
@@ -32,15 +30,13 @@ import { AppMaterialModule } from './app-material/app-material.module';
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    CoreModule,
     SharedModule,
     RecipesModule,
     ShoppingListModule,
     BrowserAnimationsModule,
     LayoutModule,
     AppMaterialModule
-  ],
-  providers: [ShoppingListService, RecipeService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptorService, multi: true}
   ],
   bootstrap: [AppComponent]
 })
